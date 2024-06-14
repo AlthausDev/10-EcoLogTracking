788370376faa9ce93e2227f1f581b90a35c9a35f
AlthausDev <samuelalthaus@gmail.com>
2024-06-14 15:00:00 +0200

    style: Mejoras visuales y agregado de elementos al esquema de la página de configuración

--- EcoLogTracking/EcoLogTracking.Client/Components/ConfigPanel.razor.cs
+++ EcoLogTracking/EcoLogTracking.Client/Components/ConfigPanel.razor.cs
@@ -1,7 +1,36 @@
-﻿namespace EcoLogTracking.Client.Components
+﻿using BlazorBootstrap;
+using Microsoft.JSInterop;
+
+namespace EcoLogTracking.Client.Components
 {
     public partial class ConfigPanel
     {
         public int DeleteFrecuencyDays { get; set; }
+        public string UserName { get; set; } = "Default";
+        public string Email { get; set; } = "Default@ecoalgo.com";
+
+
+        public string newUserName { get; set; }
+        public string newEmail { get; set; }
+        public string newPassword { get; set; }
+
+
+        record TabMessage(string Event, string ActiveTabTitle, string PreviousActiveTabTitle);
+
+        List<TabMessage> messages = new List<TabMessage>();
+
+        private void OnTabShowingAsync(TabsEventArgs args)
+            => messages.Add(new("OnShowing", args.ActiveTabTitle, args.PreviousActiveTabTitle));
+
+        private void OnTabShownAsync(TabsEventArgs args)
+            => messages.Add(new("OnShown", args.ActiveTabTitle, args.PreviousActiveTabTitle));
+
+        private void OnTabHidingAsync(TabsEventArgs args)
+            => messages.Add(new("OnHiding", args.ActiveTabTitle, args.PreviousActiveTabTitle));
+
+        private void OnTabHiddenAsync(TabsEventArgs args)
+            => messages.Add(new("OnHidden", args.ActiveTabTitle, args.PreviousActiveTabTitle));
+     
     }
+
 }
